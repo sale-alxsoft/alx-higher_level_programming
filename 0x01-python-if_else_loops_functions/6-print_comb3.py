f5fdff252e66d97b417118241fe19b123cd7ae92
sale-alxsoft <sebsale7@gmail.com>
2022-05-31 23:24:46 +0300

    string format

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 for i in range(0, 10):
     for j in range(i, 10):
-        if (i == j):
+        if (i == j or i == 8):
             continue
+        print("{}{}".format(i, j), end=", ")
+    if (i == 8):
         print("{}{}".format(i, j))

644db6480934f37c279c2fc4ea1f6e1f994e35f7
sale-alxsoft <sebsale7@gmail.com>
2022-06-03 00:52:29 +0300

    change variable name

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -4,7 +4,7 @@ def magic_calculation(a, b):
     if (a < b):
         c = add(a, b)
         for x in range(4, 6):
-            c = add(c`, x)
-        return c
+            c = add(c, x)
+        return (c)
     else:
         return (sub(a, b))

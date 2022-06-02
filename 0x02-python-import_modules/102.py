a2caa47d32d8121e43d50810cb7d3bf4a48ec757
sale-alxsoft <sebsale7@gmail.com>
2022-06-03 00:42:56 +0300

    ByteCode -> Python #3

---
+++ 0x02-python-import_modules/102.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def magic_calculation(a, b):
+    from magic_calculation_102 import add, sub
+    if (a < b):
+        for x in range(4, 6):
+            c = add(a, b)
+        return c
+    else:
+        return (sub(a, b))

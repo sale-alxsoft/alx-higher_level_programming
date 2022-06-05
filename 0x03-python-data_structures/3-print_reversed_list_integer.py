cdc645b3c71a5277105fbd3e28f4d914cd534991
sale-alxsoft <sebsale7@gmail.com>
2022-06-05 17:18:31 +0300

    reverse a list using loop

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     len_list = len(my_list)
-    if (type(my_list) == list):
-        my_list.reverse()
-        for i in my_list:
-            print("{:d}".format(i))
+    len_list -= 1
+    while len_list >= 0:
+        print("{:d}".format(my_list[len_list]))
+        len_list -= 1

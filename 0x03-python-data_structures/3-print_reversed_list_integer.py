335293f22c4615e616f0368a48fe684376e44cb5
sale-alxsoft <sebsale7@gmail.com>
2022-06-05 16:47:06 +0300

    create a list and add all elements in reverse order

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,6 +2,9 @@
 def print_reversed_list_integer(my_list=[]):
     len_list = len(my_list)
     len_list -= 1
+    new_list = []
     while (len_list >= 0):
-        print("{:d}".format(my_list[len_list]))
+        new_list.append(my_list[len_list])
         len_list -= 1
+    for i in new_list:
+        print("{:d}".format(i))

c450a5df47c8220178ab8057bbae96e04ac93eed
sale-alxsoft <sebsale7@gmail.com>
2022-06-05 17:00:02 +0300

    check if an element is integer:wq

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,9 +2,6 @@
 def print_reversed_list_integer(my_list=[]):
     len_list = len(my_list)
     len_list -= 1
-    new_list = []
-    while (len_list >= 0):
-        new_list.append(my_list[len_list])
+    while (type(my_list[len_list]) == int) and (len_list >= 0):
+        print("{:d}".format(my_list[len_list]))
         len_list -= 1
-    for i in new_list:
-        print("{:d}".format(i))

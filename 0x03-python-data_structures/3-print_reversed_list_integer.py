55d8213b3e0a8239740cd925648dbb4b85828440
sale-alxsoft <sebsale7@gmail.com>
2022-06-05 17:07:27 +0300

    check if the argument is a list

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,9 +2,7 @@
 def print_reversed_list_integer(my_list=[]):
     len_list = len(my_list)
     len_list -= 1
-    for i in my_list:
-        if (type(i) != int):
-            return
-    while (len_list >= 0):
-        print("{:d}".format(my_list[len_list]))
-        len_list -= 1
+    if type(my_list) == list:
+        while (len_list >= 0):
+            print("{:d}".format(my_list[len_list]))
+            len_list -= 1

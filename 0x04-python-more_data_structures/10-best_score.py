c510951774fe46035651e2b528bfe9f88b51689d
sale-alxsoft <sebsale7@gmail.com>
2022-06-08 21:48:32 +0300

    check if length of the dictionary is zero

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,6 +2,8 @@
 def best_score(a_dictionary):
     if (a_dictionary is None):
         return None
+    if len(a_dictionary) == 0:
+        return None
     values = list(a_dictionary.values())
     keys = list(a_dictionary.keys())
     max_val = values[0]

e192e250e1eb98c8cc1474c2a30fa3e891a2d2cf
sale-alxsoft <sebsale7@gmail.com>
2022-06-08 23:43:03 +0300

    check if the argument is dictionary

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,11 +2,8 @@
 def best_score(a_dictionary):
     if (a_dictionary is None):
         return None
-    if len(a_dictionary) == 0:
+    if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
         return None
-    for i in a_dictionary.values() or not isinstance(a_dictionary, dict):
-        if type(i) != int:
-            return None
     values = list(a_dictionary.values())
     keys = list(a_dictionary.keys())
     max_val = values[0]
